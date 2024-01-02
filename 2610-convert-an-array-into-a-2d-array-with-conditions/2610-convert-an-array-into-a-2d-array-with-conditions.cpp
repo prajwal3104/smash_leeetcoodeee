c1251ac9444c237bf7ccd8f2b451085b0626499a
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-01-03 00:25:19 +0530

    Time: 23 ms (9.84%), Space: 30 MB (70.96%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> findMatrix(vector<int>& nums) {
-        int hash[501]={0};
-        vector<vector<int>>ans;
-        vector<int> temp;
-        int mx=0;
-        for(int i=0; i<nums.size(); i++)
-        {
-            hash[nums[i]]++;
-        }
-        
-        for(int i=0; i<501; i++)
-        {
-            if(hash[i]>mx)
-            mx=hash[i];
+    vector<vector<int>> findMatrix(vector<int> &nums) {
+        vector<vector<int>> ans;
+        vector<int> temp(nums.size() + 1);
+
+        for(int i : nums) {
+            if(temp[i] >= ans.size())
+                ans.push_back({});
+            ans[temp[i]].push_back(i);
+            temp[i]++;
         }
 
-        while(mx!=0)
-        {
-        for(int i=0; i<501; i++)
-        {
-            if(hash[i]>0)
-            {
-            temp.push_back(i);
-            hash[i]--;
-            }
-        }
-        ans.push_back(temp);
-        temp.clear();
-        mx--;
-        }
         return ans;
     }
+
 };
