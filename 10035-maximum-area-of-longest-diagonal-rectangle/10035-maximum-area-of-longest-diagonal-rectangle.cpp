dcfa59cd35411dd0d4c036bdc4c38d3ac85ad5fa
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-01-07 11:40:02 +0530

    Time: 15 ms (20.00%), Space: 25.2 MB (100.00%) - LeetHub

--- 10035-maximum-area-of-longest-diagonal-rectangle/10035-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 10035-maximum-area-of-longest-diagonal-rectangle/10035-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-    int n = dimensions.size();
-        double maxDiagonal = 0;
-        int maxArea = 0;
+        int n = dimensions.size();
+        long long maxDiagonal = 0;
+        long long maxArea = 0;
 
         for (int i = 0; i < n; i++) {
             int length = dimensions[i][0];
             int width = dimensions[i][1];
 
-            double diagonal = sqrt(length * length + width * width);
+            long long diagonal = static_cast<long long>(length) * length + width * width;
 
-            if (diagonal > maxDiagonal || (diagonal == maxDiagonal && length * width > maxArea)) {
+            if (diagonal > maxDiagonal || (diagonal == maxDiagonal && static_cast<long long>(length) * width > maxArea)) {
                 maxDiagonal = diagonal;
-                maxArea = length * width;
+                maxArea = static_cast<long long>(length) * width;
             }
         }
 
-        return maxArea;
+        return static_cast<int>(maxArea);
     }
 };
