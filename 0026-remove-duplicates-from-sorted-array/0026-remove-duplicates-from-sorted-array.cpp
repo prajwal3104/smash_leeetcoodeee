a2d35bf724013029a9428e376628050216efe100
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-01-14 13:55:34 +0530

    Time: 7 ms (79.84%), Space: 19.5 MB (8.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,16 @@
-#include <set>
-#include <vector>
-
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        std::set<int> uniqueSet;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            uniqueSet.insert(nums[i]);
+    int removeDuplicates(vector<int>& nums) {
+        set<int>st;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            st.insert(nums[i]);
         }
-
-        int k = uniqueSet.size();
-        int j = 0;
-
-        for (int x : uniqueSet) {
-            nums[j++] = x;
+        int index = 0;
+        for(auto it: st){
+            nums[index] = it;
+            index++;
         }
-
-        return k;
+        return st.size();
     }
 };
