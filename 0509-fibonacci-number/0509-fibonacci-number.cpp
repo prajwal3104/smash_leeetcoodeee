dad89e66a3a814a1a6a3ba9c91634b5513915a74
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-01-28 15:35:15 +0530

    Time: 14 ms (16.97%), Space: 7.2 MB (5.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fib(n - 1) + fib(n - 2); 
+        if(n <= 1) return n;
+
+        int last = fib(n-1);
+        int slast = fib(n-2);
+        return last + slast;
         
     }
 };
