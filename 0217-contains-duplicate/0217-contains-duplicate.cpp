08181efc418e67e988c0e32a93eb05c5bc65a0e7
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-02-16 19:41:08 +0530

    Time: 148 ms (15.56%), Space: 75.8 MB (5.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> mp;
+        
+        for(int i = 0; i < n; i++){
+            mp[nums[i]]++;
+        }
+        for(auto it : mp){
+            if(it.second>=2){
+                return  true;
+            }
+        }
+        return false;
+    }
+};
