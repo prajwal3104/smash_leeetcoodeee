586f3acce2da551284da1f5a0673790aafc18a33
Prajwal Kumbar <70045720+prajwal3104@users.noreply.github.com>
2024-08-16 19:48:22 +0530

    Time: 3 ms (34.57%), Space: 10.9 MB (8.64%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,13 +5,15 @@ public:
         int n = nums.size();
         
         for(int i = 0; i < n; i++){
-            temp.push_back(nums[i]);
+           temp.push_back(nums[i]);
         }
+        
         sort(nums.begin(), nums.end());
+        
         for(int i = 0; i < temp.size(); i++){
-            if(nums[0] == temp[i]){
-                int x = i;
+            if(temp[i] == nums[0]){
                 int j = 0;
+                int x = i;
                 for(j = 0; j < n && x < temp.size(); j++){
                     if(temp[x] != nums[j]) break;
                     x++;
@@ -22,3 +24,5 @@ public:
         return false;
     }
 };
+
+
